c66ba253c1e6d589fa092ccb5970cfb82cb7936c
Samuel Kreuz <samuel.kreuz@live.com>
2021-12-02 14:05:33 +0100

    Add project files.

---
+++ 2021_AdventOfCode/Program.cs
@@ -0,0 +1,59 @@
+﻿//Pt1: 10min
+//Pt2: 20min
+
+string path = "E:/INTERAKTION/CSHARP/2021_AdventOfCode/2021_AdventOfCode/Input.txt";
+//string path = "E:/INTERAKTION/CSHARP/2021_AdventOfCode/2021_AdventOfCode/Testdata.txt";
+var intputData = File.ReadLines(path);
+var inputDataList = new List<string>(intputData);
+
+int? previousValue = null;
+int counter = 0;
+
+foreach (var line in intputData)
+{
+    if (previousValue == null)
+    {
+        previousValue = int.Parse(line);
+    }
+    else
+    {
+        if (int.Parse(line) > previousValue)
+        {
+            counter++;
+            previousValue = int.Parse(line);
+        }
+        else
+        {
+            previousValue = int.Parse(line);
+        }
+    }
+}
+
+Console.WriteLine(counter);
+
+//---------------------PART-2------------------------
+
+int pairCounter = 0;
+int? pairSums = null;
+
+for(int i = 0; i < inputDataList.Count-2; i++)
+{
+    int? currentNum = 0;
+
+    for(int j = 0; j< 3; j++)
+    {
+        currentNum += int.Parse(inputDataList[i + j]);
+    }
+
+    if(currentNum > pairSums)
+    {
+        pairCounter++;
+        pairSums = currentNum;
+    }
+    else
+    {
+        pairSums = currentNum;
+    }
+}
+
+Console.WriteLine("Paircounter: " + pairCounter);
---
+++ Day02/Program.cs
@@ -0,0 +1,53 @@
+﻿//Pt. 1 = 17min
+//Pt. 2 = 22min
+
+string path = "E:/INTERAKTION/CSHARP/2021_AdventOfCode/Day02/input.txt";
+//string path = "E:/INTERAKTION/CSHARP/2021_AdventOfCode/Day02/testData.txt";
+
+var inputData = File.ReadAllLines(path);
+
+double horizontal = 0;
+double vertical = 0;
+
+foreach (var line in inputData)
+{
+    if (line.StartsWith("forward"))
+    {
+        horizontal += Char.GetNumericValue(line.Last<char>());
+    }
+    else if (line.StartsWith("up"))
+    {
+        vertical += Char.GetNumericValue(line.Last<char>());
+    }
+    else
+    {
+        vertical -= Char.GetNumericValue(line.Last<char>());
+    }
+}
+
+Console.WriteLine("Part 1: " + horizontal * Math.Abs(vertical));
+
+
+//-------------------Part-2-------------------------
+
+double verticalSec = 0;
+double horizontalSec = 0;
+double aim = 0;
+
+foreach (var line in inputData)
+{
+    if (line.StartsWith("forward"))
+    {
+        horizontalSec += Char.GetNumericValue(line.Last<char>());
+        verticalSec += aim * Char.GetNumericValue(line.Last<char>());
+    }
+    else if (line.StartsWith("up"))
+    {
+        aim -= Char.GetNumericValue(line.Last<char>());
+    }
+    else
+    {
+        aim += Char.GetNumericValue(line.Last<char>());
+    }
+}
+Console.WriteLine("Part 2: " + horizontalSec * Math.Abs(verticalSec));
